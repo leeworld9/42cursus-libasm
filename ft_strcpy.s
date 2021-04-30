bc2aa378431e1c9a7bdc65dd78a44e2108bf7dcd
Doheon Lee <leeworld9@gmail.com>
2021-04-30 11:33:16 +0900

    edit comment

--- ft_strcpy.s
+++ ft_strcpy.s
@@ -22,8 +22,8 @@ _ft_strcpy:
 _copy:
 	cmp byte [rsi + rax], 0		; dst와 src는 각각 rdi와 rsi에 저장된다
 	je _done
-	mov dl, byte [rsi + rax]	; rdi에 값을 바로 넣을 수 없으며 rsi의 값을 dh나 dl에 임시 저장한 다음 넣는다.
-	mov byte [rdi + rax], dl
+	mov dl, byte [rsi + rax]	; rdi에 값을 바로 넣을 수 없으며 rsi의 값을 dh나 dl에 임시 저장한 다음 넣는다. 
+	mov byte [rdi + rax], dl	; 	=> 어셈블리어에서 연산은 메모리-메모리는 불가능하다. 따라서, 따로 한번 레지스터에 저장시킨 후에 다시 연산해줘야 하는 것임.
 	inc rax
 	jmp _copy
 
